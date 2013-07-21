3dfad4900d2afe54df5c43efcc6a697d01d62e8b
Adam Buechler <butchler@gmail.com>
2013-07-21 01:28:03 -0400

    Fixup project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cljs-async "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject snake "0.1.0"
+  :description "Multiplayer snake"
+  :url "http://butchler.github.com/multiplayer-snake"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "LATEST"]]
